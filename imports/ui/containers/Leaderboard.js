3d7fb35d5664cda4f0db4a8697d7ae32a3b2a778
Dakota <dakota.paige@hotmail.com>
2018-05-11 11:11:36 -0700

    Started creating the empty components for Leaderboard

--- imports/ui/containers/Leaderboard.js
+++ imports/ui/containers/Leaderboard.js
@@ -1,9 +1,25 @@
 import React, {Component} from 'react';
+import TopThree from '../components/TopThree';
+import LeaderboardList from '../components/LeaderboardList';
 
 class Leaderboard extends Component {
   render() {
     return (
-      <h1>Leaderboard!</h1>
+      <div>
+        <h1>Top Scores</h1>
+        <ul>
+          <li>General</li>
+          <li>Computers</li>
+          <li>Sciences & Nature</li>
+          <li>Film</li>
+        </ul>
+        <TopThree />
+        <LeaderboardList />
+      </div>
     )
   }
 }
+
+// feed in just the top 3 user scores into TopThree
+// feed in the rest to LeaderboardList
+// the ul will have to be pulling from listed categories rather than hard-coded
