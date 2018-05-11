8aecfbe893543f82c91d295990af08be2dac21e9
Dakota <dakota.paige@hotmail.com>
2018-05-11 13:41:12 -0700

    Added user identification

--- client/main.js
+++ client/main.js
@@ -3,12 +3,14 @@ import Question from "../imports/ui/containers/Question";
 import Question2 from "../imports/ui/containers/Question-2";
 import Question3 from "../imports/ui/containers/Question-3";
 import Question4 from "../imports/ui/containers/Question-4";
+import { withTracker } from 'meteor/react-meteor-data';
 import { Meteor } from "meteor/meteor";
 import HomePage from "../imports/ui/containers/HomePage";
 import Score from "../imports/ui/containers/Score";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import MainMenu from "../imports/ui/components/MainMenu";
 import ReactDOM from "react-dom";
+import AccountsUIWrapper from '../imports/ui/components/AccountsWrapper';
 
 class App extends Component {
   constructor() {
@@ -16,13 +18,18 @@ class App extends Component {
     this.state = {
       // isClicked: false
     };
+
   }
 
   render() {
+    console.log(this.props);
     let buttonClicked = () =>
       this.setState({ isClicked: !this.state.isClicked });
     return (
       <div className="App" style={{ width: "90%" }}>
+        <div className="login-wrapper">
+          <AccountsUIWrapper />
+        </div>
         <Router>
           <div>
             <MainMenu />
@@ -41,6 +48,15 @@ class App extends Component {
   }
 }
 
+const AppContainer = withTracker(() => {
+  return {
+    currentUser: Meteor.user(),
+    currentUserId: Meteor.userId()
+};
+})(App);
+
+
+
 Meteor.startup(() => {
-  ReactDOM.render(<App />, document.getElementById("root"));
+  ReactDOM.render(<AppContainer />, document.getElementById("root"));
 });
