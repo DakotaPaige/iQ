72d429fe8dec02dcd056b9df041d021bb37ba716
Calvin Ting <calvinting@protonmail.com>
2018-05-14 11:00:00 -0700

    tried adding score to work with points

--- imports/start-up/server/fixture.js
+++ imports/start-up/server/fixture.js
@@ -1,3 +1,11 @@
 import { Meteor } from "meteor/meteor";
 import { Questions } from "../../api/questions";
 import { Scores } from "../../api/scores";
+
+// Meteor.startup(() => {
+//   if (Questions.find().count() === 0) {
+//     .insert({
+//       user: Meteor.userId()
+//     });
+//   }
+// });
