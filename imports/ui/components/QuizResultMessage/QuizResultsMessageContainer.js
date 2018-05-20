39b3ad8adafeaf9d1d54716ebbdd945886c031bf
Calvin Ting <calvinting@protonmail.com>
2018-05-19 19:47:36 -0700

    add bar chart for results after question only working for computer science

--- imports/ui/components/QuizResultMessage/QuizResultsMessageContainer.js
+++ imports/ui/components/QuizResultMessage/QuizResultsMessageContainer.js
@@ -6,6 +6,15 @@ import { Scores } from "../../../api/scores";
 import { Mongo } from "meteor/mongo";
 import { withTracker } from "meteor/react-meteor-data";
 import "./styles.css";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
 
 let decodeEntities = encodedString => {
   var translate_re = /&(nbsp|amp|quot|lt|gt);/g;
@@ -31,25 +40,36 @@ const QuestionResult = props => {
   if (props.users && props.users) {
     numberOfLoggedIn = props.users.filter(user => user.status.online).length;
   }
+  let percentageWon =
+    props.questionAnswer.correctscore / numberOfLoggedIn * 100;
+  let percentageLoss =
+    (numberOfLoggedIn - props.questionAnswer.correctscore) /
+    numberOfLoggedIn *
+    100;
+  console.log(percentageWon);
+  const data = [
+    { name: "Won", uv: percentageWon },
+    { name: "Loss", uv: percentageLoss }
+  ];
   return (
     <div>
       {props.isCorrectAnswer == true ? (
         <div>
-          <h1>
-            Good Job
-            {numberOfLoggedIn}
-          </h1>
+          <h1>Good Job</h1>
           {/* {console.log(props.numberofusers)} */}
-          <h1>
-            Percentage Won:
-            {props.questionAnswer.correctscore / numberOfLoggedIn * 100}%
-          </h1>
-          <h1>
-            Percentage Loss:
-            {(numberOfLoggedIn - props.questionAnswer.correctscore) /
-              numberOfLoggedIn *
-              100}%
-          </h1>
+          <BarChart
+            width={600}
+            height={300}
+            data={data}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="uv" fill="#82ca9d" />
+          </BarChart>
         </div>
       ) : (
         <div>
@@ -57,19 +77,21 @@ const QuestionResult = props => {
           <h1>The correct answer is</h1>
           <h1 className="answer">
             {decodeEntities(props.questionAnswer.correct)}
-            {numberOfLoggedIn}
             {/* {console.log(props.numberofusers)} */}
           </h1>
-          <h1>
-            Percentage Won:
-            {props.questionAnswer.correctscore / numberOfLoggedIn * 100}%
-          </h1>
-          <h1>
-            Percentage Loss:
-            {(numberOfLoggedIn - props.questionAnswer.correctscore) /
-              numberOfLoggedIn *
-              100}%
-          </h1>
+          <BarChart
+            width={600}
+            height={300}
+            data={data}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="uv" fill="#82ca9d" />
+          </BarChart>
         </div>
       )}
       <button onClick={props.showQuestions}> click here</button>
