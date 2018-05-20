db40f6ec7e0b1500aeef3175e8a7ecdd9ac6fc33
Dakota <dakota.paige@hotmail.com>
2018-05-20 12:35:37 -0700

    fixed math for leaderboard sorting and made it so all time shows when you load up the page

--- imports/ui/containers/Leaderboard/Leaderboard.js
+++ imports/ui/containers/Leaderboard/Leaderboard.js
@@ -7,86 +7,19 @@ import { Users } from "../../../api/users";
 import { Button, ButtonGroup } from "reactstrap";
 import { withTracker } from "meteor/react-meteor-data";
 
-// const mockUserData = [
-//   {
-//     emails: [
-//       {
-//         address: 'cat@cats.com',
-//         verified: false
-//       }
-//     ],
-//     profile: {
-//       points: [
-//         {category: 'computer-science', points: 0},
-//         {category: 'film', points: 10},
-//         {category: 'general-knowledge', points: 7},
-//         {category: 'science-nature', points: 15},
-//         {category: 'all', points: 32}
-//       ],
-//       username: 'Cats'
-//     }
-//   },
-//   {
-//     emails: [
-//       {
-//         address: 'test@test.com',
-//         verified: false
-//       }
-//     ],
-//     profile: {
-//       points: [
-//         {category: 'computer-science', points: 5},
-//         {category: 'film', points: 5},
-//         {category: 'general-knowledge', points: 5},
-//         {category: 'science-nature', points: 5},
-//         {category: 'all', points: 20}
-//       ],
-//       username: 'Tester'
-//     }
-//   },
-//   {
-//     emails: [
-//       {
-//         address: 'blah@blah.com',
-//         verified: false
-//       }
-//     ],
-//     profile: {
-//       points: [
-//         {category: 'computer-science', points: 10},
-//         {category: 'film', points: 10},
-//         {category: 'general-knowledge', points: 10},
-//         {category: 'science-nature', points: 10},
-//         {category: 'all', points: 40}
-//       ],
-//       username: 'Hello!'
-//     }
-//   },
-//   {
-//     emails: [
-//       {
-//         address: 'user3@blah.com',
-//         verified: false
-//       }
-//     ],
-//     profile: {
-//       points: [
-//         {category: 'computer-science', points: 6},
-//         {category: 'film', points: 6},
-//         {category: 'general-knowledge', points: 6},
-//         {category: 'science-nature', points: 6},
-//         {category: 'all', points: 24}
-//       ],
-//       username: 'User3'
-//     }
-//   }
-// ]
-
 function compareAllPoints(a, b) {
-  return (
-    a.profile.points[4].points / a.profile.gamesPlayed -
-    b.profile.points[4].points / b.profile.gamesPlayed
-  );
+  if (a.profile.gamesPlayed === 0 && b.profile.gamesPlayed === 0) {
+    return a.profile.points[4].points - b.profile.points[4].points;
+  } else if (a.profile.gamesPlayed === 0) {
+    return 0 - b.profile.points[4].points / b.profile.gamesPlayed;
+  } else if (b.profile.gamesPlayed === 0) {
+    return a.profile.points[4].points / a.profile.gamesPlayed - 0;
+  } else {
+    return (
+      a.profile.points[4].points / a.profile.gamesPlayed -
+      b.profile.points[4].points / b.profile.gamesPlayed
+    );
+  }
 }
 
 function compareComputerPoints(a, b) {
@@ -105,18 +38,6 @@ function compareSciencePoints(a, b) {
   return a.profile.points[3].points - b.profile.points[3].points;
 }
 
-// const sortedAllPoints = mockUserData.sort(compareAllPoints).slice().reverse();
-// const sortedComputerPoints = mockUserData.sort(compareComputerPoints).slice().reverse();
-// const sortedFilmPoints = mockUserData.sort(compareFilmPoints).slice().reverse();
-// const sortedGeneralPoints = mockUserData.sort(compareGeneralPoints).slice().reverse();
-// const sortedSciencePoints = mockUserData.sort(compareSciencePoints).slice().reverse();
-
-// console.log('this is sorted all:', sortedAllPoints);
-// console.log('This is sorted Computer: ', sortedComputerPoints);
-// console.log('This is sorted Film: ', sortedFilmPoints);
-// console.log('This is sorted General: ', sortedGeneralPoints);
-// console.log('This is sorted Science: ', sortedSciencePoints);
-
 class Leaderboard extends Component {
   constructor() {
     super();
@@ -124,6 +45,14 @@ class Leaderboard extends Component {
       sortedUserData: [],
       currentIndex: 0
     };
+  }
+
+  componentDidMount() {
+    const sortedAllPoints = this.props.users
+      .sort(compareAllPoints)
+      .slice()
+      .reverse();
+    this.setState({ sortedUserData: sortedAllPoints, currentIndex: 4 });
   }
 
   getAllTime = () => {
@@ -139,6 +68,7 @@ class Leaderboard extends Component {
       .sort(compareFilmPoints)
       .slice()
       .reverse();
+    console.log("THIS IS SORTED FILM: ", sortedFilmPoints);
     this.setState({ sortedUserData: sortedFilmPoints, currentIndex: 1 });
   };
 
