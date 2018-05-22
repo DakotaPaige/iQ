6306d33e6fd88ff462e8d78b04fd3e522ab19f95
Calvin Ting <calvinting@protonmail.com>
2018-05-22 09:39:55 -0700

    redirect is added, not 100% working but it is redirecting without reloading the page

--- imports/ui/containers/HomePage/HomePage.js
+++ imports/ui/containers/HomePage/HomePage.js
@@ -3,7 +3,7 @@ import MainMenu from "../../components/MainMenu";
 import TextLoop from "react-text-loop";
 import { withTracker } from "meteor/react-meteor-data";
 import { Questions } from "../../../api/questions";
-import { BrowserRouter, Link, withRouter } from "react-router-dom";
+import { BrowserRouter, Link, withRouter, Redirect } from "react-router-dom";
 import history from "../../../history";
 
 const HomePage = props => {
@@ -14,15 +14,13 @@ const HomePage = props => {
     </div>
   ) : (
     <div>
-      {props.questionAnswer && props.questionAnswer.path !== "/"
-        ? props.currentUser &&
-          props.currentUser.profile.hasOwnProperty("superuser")
-          ? null
-          : props.history &&
-            props.history.push(
-              `/${props.questionAnswer && props.questionAnswer.path}`
-            )
-        : null}
+      {props.questionAnswer && props.questionAnswer.path !== "/" ? (
+        props.currentUser &&
+        props.currentUser.profile.hasOwnProperty("superuser") ? null : (
+          <Redirect to={props.questionAnswer && props.questionAnswer.path} />
+        )
+      ) : null}
+
       <h1 className="textloop">
         <TextLoop>
           <div>Have Fun!</div>
